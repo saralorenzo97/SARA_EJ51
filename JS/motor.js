6438f6079a27677ccc1d3eb42093aff354668ee2
saralorenzo97 <saralorenzosegovia@gmail.com>
2021-05-27 13:13:40 +0200

    precio

--- JS/motor.js
+++ JS/motor.js
@@ -1,4 +1,4 @@
-var compra=1
+var contador=0;
 
 var movil=[
     {
@@ -26,15 +26,71 @@ var movil=[
         pequeno:125,
         mediano:225,
         grande:325
-    }
+    },
+
+    {
+        imagen:"IMG/sonyexperia L3 1.png",
+        imagen2:"IMG/sonyexperia L3 2.png",
+        nombre:"Sony Experia L3",
+        pequeno:125,
+        mediano:225,
+        grande:325
+    },
+
+    {
+        imagen:"IMG/nokia71 1.png",
+        imagen2:"IMG/nokia71 2.png",
+        nombre:"Nokia 71",
+        pequeno:125,
+        mediano:225,
+        grande:325
+    },
+
+    {
+        imagen:"IMG/galaxynote10lite 1.png",
+        imagen2:"IMG/galaxynote10lite 2.png",
+        nombre:"Samsung Galaxy NOTE 10 Lite",
+        pequeno:150,
+        mediano:250,
+        grande:350
+    },
+
+    {
+        imagen:"IMG/lg-v30-frontal.png",
+        imagen2:"IMG/lg-v30-trasera.png",
+        nombre:"LG V30 ",
+        pequeno:150,
+        mediano:250,
+        grande:350
+    },
+
+    {
+        imagen:"IMG/alcatel-1x-2019 1.png",
+        imagen2:"IMG/alcatel-1x-2019.png",
+        nombre:"LG V30 ",
+        pequeno:150,
+        mediano:250,
+        grande:350
+    },
+
+    {
+        imagen:"IMG/iphone-11 1.png",
+        imagen2:"IMG/iphone-11 2.png",
+        nombre:"LG V30 ",
+        pequeno:150,
+        mediano:250,
+        grande:350
+    },
 ];
+
+lista=[];
 
 pintar();
 function pintar(){
     for(let x=0; x<movil.length; x++){
         document.getElementById("contenedor").innerHTML+=` 
     <div class="caja">
-        <div id="rojo${x}" class="rojo">${movil[x].pequeno}</div>
+        <div id="rojo${x}" class="rojo">${movil[x].pequeno}€</div>
         <div class="foto">
             <img src="${movil[x].imagen}" alt="" id="mifoto${x}"
         </div>
@@ -53,7 +109,7 @@ function pintar(){
             </div>
 
             <div class="compra">
-                <button onclick="comprar() id="comprar">Comprar</button>
+                <button onclick="anadircarrito(${x})" id="comprar">Comprar</button>
             </div>
         </div>
     </div>`
@@ -72,18 +128,48 @@ function cambiafoto(posicion) {
 
 function cambiaprecio(posicion) {
     if(document.getElementById(`tamanop${posicion}`).checked){
-        document.getElementById(`rojo${posicion}`).innerHTML=movil[posicion].pequeno
+        document.getElementById(`rojo${posicion}`).innerHTML=movil[posicion].pequeno + "€";
     }
     if(document.getElementById(`tamanom${posicion}`).checked){
-        document.getElementById(`rojo${posicion}`).innerHTML=movil[posicion].mediano
+        document.getElementById(`rojo${posicion}`).innerHTML=movil[posicion].mediano + "€";
     }
     if(document.getElementById(`tamanog${posicion}`).checked){
-        document.getElementById(`rojo${posicion}`).innerHTML=movil[posicion].grande
+        document.getElementById(`rojo${posicion}`).innerHTML=movil[posicion].grande + "€";
     }
 }
 
-function comprar(posicion) {
-    for(x=0; x<compra; x++){
-        document.getElementById("numero").innerHTM=compra
+function anadircarrito(posicion){
+    contador++;
+    document.getElementById("numero").innerHTML=contador;
+    alert("añadido " + movil[posicion].nombre);
+    if(document.getElementById(`tamanop${posicion}`).checked){
+        valor=movil[posicion].pequeno;
+        tamanox="32GB"
+    }
+    if(document.getElementById(`tamanom${posicion}`).checked){
+        valor=movil[posicion].pequeno;
+        tamanox="64GB"
+    }
+    if(document.getElementById(`tamanog${posicion}`).checked){
+        valor=movil[posicion].pequeno;
+        tamanox="128GB"
+    }
+    lista.push({nombre:movil[posicion].nombre,precio:valor})
+}
+
+function listarCarrito() {
+    document.getElementById('contenedorCarrito').innerHTML="";
+    for(let x=0;x<lista.length;x++){
+        document.getElementById('contenedorCarrito').innerHTML+=`<div class="cajaCarrito">
+                                                                <div>${lista[x].nombre} </div>
+                                                                <div>${lista[x].precio} </div>
+                                                                 </div>` ;
     }
 }
+
+// function valortotal(posicion) {
+//     document.getElementById('preciototal').innerHTML="";
+//     if(document.getElementById(`tamanop${posicion}`).checked){
+//         total=
+//     }
+// }
